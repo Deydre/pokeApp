f043f9125770e5991148f83cd5ed328585121768
Deydre <deydrealonso@gmail.com>
2024-11-23 16:04:45 +0100

    data from API shaped

--- src/components/Main/ListaPokemon/Card/Card.jsx
+++ src/components/Main/ListaPokemon/Card/Card.jsx
@@ -3,12 +3,17 @@ import React from "react";
 const Card = (pokemon) => {
   const {name, url} = pokemon.data;
   console.log(pokemon)
+  // TRATAMIENTO  DE DATOS ----------------
+  // Ponemos el nombre en mayúscula
+  let nameUppercase = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
   // Sacamos el ID de la url que nos viene de la llamada a la API de cada Pokemon
   const pokemonId = url.split("/")[url.split("/").length - 2];
   // Guardamos en una variable la URL para pasársela al src de la img
   const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+
+
   return <article className="card">
-      <h3>{name}</h3>
+      <h3>{nameUppercase}</h3>
       <img src={imageUrl} alt={name} />
   </article>;
 };
